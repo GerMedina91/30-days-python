19d96cfa17c64456800892a768bbccf9b43728e7
German Medina <64096992+GerMedina91@users.noreply.github.com>
2022-07-10 11:54:09 -0300

    .

--- 30DaysOfPython/day_3/exercises.py
+++ 30DaysOfPython/day_3/exercises.py
@@ -39,42 +39,59 @@ print(f'The circumference of the circle is {c_circumference}')
 print('-----------------------------------------------')
 
 #Calculate the slope, x-intercept and y-intercept of y = 2x -2
+print('Slope is : '+ str(2))
+print('x-intercept is '+ str(0))
+print('y-intercept is '+ str(-2))
 
-def getXandYintercept(P, Q):
- 
-    a = P[1] - Q[1]
-    b = P[0] - Q[0]
-     
-    # if line is parallel to y axis
-    if b == 0:
-        print(P[0])         # x - intercept will be p[0]
-        print("infinity")   # y - intercept will be infinity
-        return
-     
-    # if line is parallel to x axis
-    if a == 0:
-        print("infinity")     # x - intercept will be infinity
-        print(P[1])           # y - intercept will be p[1]
-        return
-     
-     
-         
-    # Slope of the line
-    m = a / b
-     
-    # y = mx + c in where c is unknown
-    # Use any of the given point to find c
-    x = P[0]
-    y = P[1]
-    c = y-m * x
-     
-    # For finding the x-intercept put y = 0
-    y = 0
-    x =(y-c)/m
-    print(x)
-     
-    # For finding the y-intercept put x = 0
-    x = 0
-    y = m * x + c
-    print(y)
- 
+print('-----------------------------------------------')
+
+#Slope is (m = y2-y1/x2-x1). Find the slope between point (2, 2) and point (6,10)
+def slope(x1, y1, x2, y2):
+    """
+    It takes four numbers as input and returns the slope of the line that passes through the points
+    (x_1, y_1)$ and (x_2, y_2)
+    
+    :param x1: x-coordinate of the first point
+    :param y1: y-coordinate of the first point
+    :param x2: x-coordinate of the second point
+    :param y2: the y-coordinate of the second point
+    :return: The slope of the line between the two points.
+    """
+    return (y2-y1)/(x2-x1)
+print(f'The slope is {slope(2,2,6,10)}')
+
+print('-----------------------------------------------')
+
+#Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value y is 0.
+def calculo(x = 0):
+    return x**2 + 6*x + 9
+
+print(f'Calc x**2 + 6*x + 9 when x is 0: {calculo(0)}')
+
+print('-----------------------------------------------')
+
+#Use and operator to check if 'on' is found in both 'python' and 'jargon'
+py = len('python')
+jar = len('jargon')
+print(py < jar)
+
+print('-----------------------------------------------')
+
+#I hope this course is not full of jargon. Use in operator to check if jargon is in the sentence.
+sentence = 'I hope this course is not full of jargon'
+print('jargon' in sentence)
+
+print('-----------------------------------------------')
+
+#There is no 'on' in both dragon and python
+print('on' in 'dragon' and 'on' in 'python')
+
+print('-----------------------------------------------')
+
+#Find the length of the text python and convert the value to float and convert it to string
+len_py = str(float(len('python')))
+print(len_py)
+
+#Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or not using python?
+
+
